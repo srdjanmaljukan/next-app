c9bcb494ff8a6055c83840ede57a650b53bb85bf
srdjanmaljukan <srdjanmaljukan96@gmail.com>
2024-01-06 15:52:02 +0100

    Auth

---
+++ middleware.ts
@@ -0,0 +1,10 @@
+import middleware from "next-auth/middleware";
+
+export default middleware
+
+export const config = {
+    // *: zero or more parameters
+    // +: one or more parameters
+    // ?: zero or one
+    matcher: ["/dashboard/:path*"]
+}
